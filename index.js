09be7ab061cd1e83f9bce22aa063cce8b9c68a91
TobyFlan <toby.flanagan@hotmail.co.uk>
2023-09-18 16:13:18 +0100

    exc 3.15 complete

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ var morgan = require('morgan')
 
 const Person = require('./models/person')
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
+
 
 
 morgan.token('new-person', (req, res) => {
@@ -17,28 +18,6 @@ morgan.token('new-person', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :new-person'))
 
-let persons = [
-    { 
-    id: 1,
-    name: "Arto Hellas", 
-    number: "040-123456"
-    },
-    { 
-    id: 2,
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-    },
-    { 
-    id: 3,
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-    },
-    { 
-    id: 4,
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-    }
-]
 
 //return all people saved to the server
 app.get(`/api/persons`, (request, response) => {
@@ -71,12 +50,11 @@ app.get(`/api/persons/:id`, (request, response) => {
 //delete a person from the phonebook
 app.delete('/api/persons/:id', (request, response) => {
 
-    const id = Number(request.params.id);
-    
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
-
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    }).catch(error => {
+        console.log(error);
+    })
 })
 
 //add a new person to the phonebook database
